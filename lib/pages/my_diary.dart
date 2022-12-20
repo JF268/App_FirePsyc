afeea94a2e85c9116465743af638fb059fa6b943
franco <withamadness@gmail.com>
2022-12-20 11:56:55 -0500

    creando elementos para el diario personal

--- lib/pages/my_diary.dart
+++ lib/pages/my_diary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:psycjfapp/widgets/divider_widget.dart';
 
 
 class MyDiary extends StatelessWidget {
@@ -9,7 +10,7 @@ class MyDiary extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           flexibleSpace: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               gradient: LinearGradient(
                 colors: [
                   Colors.teal, Colors.white70
@@ -19,14 +20,77 @@ class MyDiary extends StatelessWidget {
               ),
             ),
           ),
-          title: Text("Añadir entrada", style: TextStyle(fontWeight: FontWeight.w500),),
-          leading: Icon(Icons.menu_book_rounded,size: 40),
+          title: const Text("Añadir entrada", style: TextStyle(fontWeight: FontWeight.w500),),
+          leading: const Icon(Icons.menu_book_rounded,size: 40),
           actions: <Widget> [
-            IconButton(onPressed: (){}, icon: const Icon(Icons.border_color,color: Colors.black45,))
+            IconButton(onPressed: (){
+              //Codigo para añadir entrada al diario
+            }, icon: const Icon(Icons.border_color,color: Colors.black45,))
           ],
         ),
-        body: Center(
-          child: Text("Hola a todos"),)
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                divider10(),
+                Text("Entradas para mi diario personal",style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87.withOpacity(0.55),
+                ),),
+                divider6(),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Card(
+                        child: ListTile(
+                          leading: Padding(
+                            padding: EdgeInsets.only(top: 12.0),
+                            child: Text("10/12/2022"),
+                          ),
+                          autofocus: true,
+                          title: Text('Titulo de historia 1'),
+                          subtitle:
+                          Text('Hoy obtuve un nuevo trabajo como desarrollador y las horas laborales son bastante ligeras'),
+                          trailing: Icon(Icons.more_vert_rounded),
+                          isThreeLine: true,
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Padding(
+                            padding: EdgeInsets.only(top: 12.0),
+                            child: Text("11/12/2022"),
+                          ),
+                          autofocus: true,
+                          title: Text('Titulo de historia 2'),
+                          subtitle:
+                          Text("Aqui puede entrar otra historia que desees añadir al dirio"),
+                          trailing: Icon(Icons.more_vert_rounded),
+                          isThreeLine: true,
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Padding(
+                            padding: EdgeInsets.only(top: 12.0),
+                            child: Text("15/12/2022"),
+                          ),
+                          autofocus: true,
+                          title: Text('Titulo de historia 3'),
+                          subtitle:
+                          Text('Hoy no fue un buen dia y tuve una conflicto con un amigo de la universidad'),
+                          trailing: Icon(Icons.more_vert_rounded),
+                          isThreeLine: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
---
+++ lib/widgets/divider_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Widgets para altura
+Widget divider3() => const SizedBox(height: 3);
+Widget divider6() => const SizedBox(height: 6);
+Widget divider10() => const SizedBox(height: 10);
+Widget divider20() => const SizedBox(height: 20);
+//Widgets para ancho
+Widget dividerW3() => const SizedBox(width: 3);
+Widget dividerW6() => const SizedBox(width: 6);
+Widget dividerW10() => const SizedBox(width: 10);
+
